bc474590f103affca33a2cc64bf927f25ec27f36
JoeContreras <joe.contreras888@gmail.com>
2021-09-29 19:51:24 -0500

    mui theme fix 3

--- src/ui/Footer.js
+++ src/ui/Footer.js
@@ -1,23 +1,25 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   footer: {
     backgroundColor: theme.palette.common.blue,
     width: "100%",
     zIndex: 1302,
-    position: "relative"
+    position: "relative",
+    height: "15.5em",
   },
   adornment: {
     width: "25em",
+    height: "15.5em",
     verticalAlign: "bottom",
     [theme.breakpoints.down("md")]: {
-      width: "21em"
+      width: "21em",
     },
     [theme.breakpoints.down("xs")]: {
-      width: "15em"
-    }
-  }
+      width: "15em",
+    },
+  },
 }));
 
 export default function Footer(props) {
