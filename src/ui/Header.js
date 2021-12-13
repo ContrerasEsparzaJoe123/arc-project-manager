a820d36a98ced673bc13a5fb07c3f86ff1b85cda
joec <joe.contreras888@gmail.com>
2021-12-13 17:32:04 -0600

    Changed font size and position in Amp Development

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -70,16 +70,16 @@ export default function Header(props) {
             <path d="M448.07-1l-9.62 17.24-8.36 14.96L369.93 139H-1V-1z" />
             <path className="st0" d="M-1 139h479.92v.01H-1z" />
             <text
-              transform="translate(261.994 65.233)"
+              transform="translate(251.994 65.233)"
               className="st1 st2"
-              fontSize="57"
+              fontSize="43"
             >
               Amp
             </text>
             <text
               transform="translate(17.692 112.015)"
               className="st1 st2"
-              fontSize="54"
+              fontSize="46"
             >
               Development
             </text>
